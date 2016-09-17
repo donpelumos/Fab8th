c872b1e92aa62f2d13245cb6223a80b15560f7cb
Don Pelumos <donpelumos@gmail.com>
2016-09-17 10:51:43 +0100

    firt

--- Fab8th/backup/get.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "Pelumi,Leke,Tunmise,Mayokun";
-?>
--- Fab8th/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-//session_save_path("/home/users/web/b1621/ipg.fab8thproductionscom/cgi-bin/tmp");
-	session_start();
-	session_destroy();
-	header("Location: admin.php");
-?>
